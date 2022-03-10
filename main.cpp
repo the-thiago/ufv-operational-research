3b366da51781e239ee628b9b36dd1b5c7d3b2b9a
Thiago <thiago.moreira.souza@gmail.com>
2022-03-10 00:17:38 -0300

    Two first constraints

--- main.cpp
+++ main.cpp
@@ -21,57 +21,155 @@ int main()
     cout << endl << "Processando..." << endl;
 
     IloEnv env;             // Declara o ambiente
-    IloModel Model(env);     // Declara o modelo dentro do ambiente
+    IloModel Model(env);    // Declara o modelo dentro do ambiente
 
     // Dados do modelo
     // todo
-    int CSTv = 10;
-    int PEN = 10;
-    int numDePassageiros = 10;
-    int numDeVoos = 10;
+
+    int numDePassageiros = 2;
+    int numDeVoos = 2;
+    int ATR = 100;
+    int PEN = 100;
+    int* CSTv = new int[numDeVoos] { 0, 0 };
+    int* HCv = new int[numDeVoos] { 0, 0 };
+    int* ADv = new int[numDeVoos] { 0, 0 };
+    int* AOv = new int[numDeVoos] { 0, 0 };
+    int* CHEGp = new int[numDePassageiros] { 0, 0 };
+    int* DESTp = new int[numDePassageiros] { 0, 0 };
 
 
     // Variaveis de decisao binarias
-    IloNumVarArray passagNegligenciado(env, numDePassageiros, 0, 1, IloNumVar::Bool);
-    Var2D passagEmVoo(env, numDePassageiros);
+    // Variavel booleana: passageiro p foi negligenciado na realocacao?
+    IloNumVarArray x(env, numDePassageiros, 0, 1, IloNumVar::Bool);
+    // Variavel booleana de duas dimensoes: passageiro p está alocado ao voo v?
+    Var2D y(env, numDePassageiros);
 
     for(int p = 0; p < numDePassageiros; p++)
     {
-        passagEmVoo[p] = IloNumVarArray(env, numDeVoos, 0, 1, IloNumVar::Bool);
+        y[p] = IloNumVarArray(env, numDeVoos, 0, 1, IloNumVar::Bool);
     }
 
-    // Funcao objetivo
+    // Funcao objetivo (1) ===============================================================
     IloExpr custoRealocacao(env);
+
+    for(int p = 0; p < numDePassageiros; p++)
+    {
+        for(int v = 0; v < numDeVoos; v++)
+        {
+            custoRealocacao += (CSTv[v] * y[p][v]);
+        }
+    }
+
     IloExpr penalidadeNeglig(env);
+
+    for(int p = 0; p < numDePassageiros; p++)
+    {
+        penalidadeNeglig += (PEN * x[p]);
+    }
+
     IloExpr passagInsatisfacao(env);
 
     for(int p = 0; p < numDePassageiros; p++)
     {
         for(int v = 0; v < numDeVoos; v++)
         {
-            custoRealocacao += CSTv * passagEmVoo[p][v];
+            if(DESTp[p] == ADv[v])
+            {
+                passagInsatisfacao += (ATR * (HCv[v] - CHEGp[p]) * y[p][v]);
+            }
         }
     }
-    Model.add(IloMinimize(env, custoRealocacao));
+    Model.add(IloMinimize(env, custoRealocacao + penalidadeNeglig + passagInsatisfacao));
 
+    // Restricoes =======================================================================
+    // Restricao (2) garante que cada passageiro saia do aeroporto de origem ou, então,
+    // que seja indicada a sua não realocação
     for(int p = 0; p < numDePassageiros; p++)
     {
-        penalidadeNeglig += PEN * passagNegligenciado[p];
+        IloExpr expr(env);
+        for(int v = 0; v < numDeVoos; v++)
+        {
+            if (AOv[v] == 0)
+            {
+                expr += y[p][v];
+            }
+        }
+        expr += x[p];
+        Model.add(expr == 1);
     }
-    Model.add(IloMinimize(env, penalidadeNeglig));
 
+    // Restricao (3) impõem a condição que cada passageiro realocado chegue a seu
+    // destino final
+    for(int p = 0; p < numDePassageiros; p++)
+    {
+        IloExpr expr(env);
+        for(int v = 0; v < numDeVoos; v++)
+        {
+            if (ADv[v] == DESTp[p])
+            {
+                expr += y[p][v];
+            }
+        }
+        expr += x[p];
+        Model.add(expr == 1);
+    }
+
+    // Restricao (4) assegura a continuidade de um itinerário durante as conexões
+    // entre aeroportos
+
+    // Restricao (5) certifica que um voo obedeça sua capacidade máxima
+
+    // Restricao (6) garante que um passageiro parta do aeroporto de origem somente
+    // após a hora de partida do seu itinerário original
+
+    // Restricao (7) assegura que o tempo mı́nimo entre conexõesseja respeitado
+
+    // Restricao (8) refere-se ao tempo máximo em que uma conexão pode ocorrer
+
+    // Restricao (9) e (10) evitam a criação de ciclos em um itinerário, garantindo
+    // que um passageiro não retorne a um aeroporto já visitado.
+
+    // Resolvendo o modelo
+    /*
+    IloCplex cplex(Model);
+    cplex.exportModel("ModeloExportado.lp");
+    if(!cplex.solve())
+    {
+        env.error() << "Erro ao rodar modelo!" << endl;
+        throw(-1);
+    }
+    double obj = cplex.getObjValue();
+    cout << "Valor da FO: " << obj << endl;
+
+    cout << "Valores de x:" << endl;
+    for(int p = 0; p < numDePassageiros; p++)
+    {
+        double xValue = cplex.getValue(x[p]);
+        count << "\t\t x[" << p << "] = " << xValue << endl;
+    }
+
+    cout << "Valores de y:" << endl;
     for(int p = 0; p < numDePassageiros; p++)
     {
         for(int v = 0; v < numDeVoos; v++)
         {
-            // se o destino do passageiro for diferente do destino do voo v, então break
-            passagInsatisfacao += PEN * passagNegligenciado[p]; // todo expressao errada
+            double yValue = cplex.getValue(y[p][v]);
+            count << "\t\t y[" << p << "][" << v << "] = " << yValue << endl;
         }
     }
-    Model.add(IloMinimize(env, passagInsatisfacao));
-
-    // Restricoes
-
-
+    */
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
